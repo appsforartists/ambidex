64be9eab5cc65f1bf04dc7a1cae1c7c123983e5d
Brenton Simpson <git@appsforartists.com>
2015-02-14 09:33:56 -0800

    [added] warning if undefined action or store requested

--- src/mixins/Reflux.jsx
+++ src/mixins/Reflux.jsx
@@ -6,11 +6,11 @@ var RefluxMixin = {
                                 },
 
   "getRefluxAction":            function (actionName) {
-                                  return this.context.ambidexReflux.actions[actionName];
+                                  return this.context.ambidexReflux.actions[actionName] || console.warn(`Reflux action ${ actionName } was not found.`);
                                 },
 
   "getRefluxStore":             function (storeName) {
-                                  return this.context.ambidexReflux.stores[storeName];
+                                  return this.context.ambidexReflux.stores[storeName]   || console.warn(`Reflux store ${ storeName } was not found.`);
                                 }
 };
 
